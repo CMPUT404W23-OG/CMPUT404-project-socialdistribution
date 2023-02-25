db997874c57e6794c6bf8c0ee0091ce9e390aa7a
vardansaini <vardan1@ualberta.ca>
2023-02-25 00:32:59 -0700

    Minor changes for profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { CssBaseline } from "@mui/material";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
 import Header from "./components/Header";
 
 function App() {
@@ -26,7 +27,7 @@ function App() {
 
             <Route path="login" element={<LoginPage />} />
             <Route path="signup" element={<SignUpPage />} />
-
+            <Route path="profile" element={<ProfilePage />}/>
           </Routes>
         </AuthProvider>
       </Router>
