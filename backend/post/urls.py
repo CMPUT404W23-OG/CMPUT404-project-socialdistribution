6cdfaac326154027f7571e091588a04ffeb33dde
Matthew Sheydwasser <mattsheydwasser@gmail.com>
2023-02-26 21:47:00 -0700

    Update urls.py

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostView
 
 urlpatterns = [
-    path('posts', PostView.as_view, name='posts'),
-    path('posts/post', PostView.newPost, name='newposts'),
-    path('posts/<int:pk>', PostView.editPost, name='editposts')
+    path('posts', PostView.as_view(), name='posts'),
+    path('posts/post', PostView.as_view(), name='newposts'),
+    path('posts/<int:pk>', PostView.as_view(), name='editposts')
 ]
