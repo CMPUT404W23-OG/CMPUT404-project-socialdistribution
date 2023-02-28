e170aae482e50f9c7803eaa297bb4b4bd432a420
mattsheydwasser <sheydwas@ualberta.ca>
2023-02-28 10:03:33 -0700

    homepage gets posts and displays them

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,7 +6,8 @@ import BasePath from "../config/BasePath";
 
 function CreateArray() {
 
-  
+  const [posts, setPosts] = useState([])
+  const [page, setPage] = useState(0)
   const data = [
     { 
       "author": "John Doe", 
@@ -30,45 +31,53 @@ function CreateArray() {
     }
   ]
   
-  // let response = fetch(BasePath + "/posts", {
-  //   method: "GET",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // });
-    
-  const listItems = data.map(
-    (element) => {
-      return (
+  function getData() {
+    fetch(BasePath+'/posts/all/', {
+      method: 'GET',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then((res) => res.json())
+    .then((result) => {
+      setPosts((result))
+      setPage(page+1)
+    })
+  }
+  if (page < 1) {
+    getData()
+  }
+  console.log(posts)
+  
+  const listItems = posts.map((post) =>
+      <Box
+      sx={{
+        display: 'flex',
+        flex: '1 1 auto',
+        flexDirection: 'column',
+        width: '100%',
+        margin:'15px',
+        border: '2px solid black'
+      }}>
+        <Container maxWidth="md">
+        <Card className={post.id}>
+          <h1>{post.title}</h1>
+          <h2>{post.author}</h2>
+          <p>{post.body}</p>
+          
+        </Card>
+      </Container>
+    </Box>
 
-        <Box
-          sx={{
-            display: 'flex',
-            flex: '1 1 auto',
-            flexDirection: 'column',
-            width: '100%',
-          }}
-        >
-          <Container maxWidth="md">
-          <Card className={element.author}>{element.content}</Card>
-          </Container>
-        </Box>
-        
-        // <ul type='disc'>
-        //   <li style={{ fontWeight: 'bold', color:'red'}}>{element.author}</li>
-        //   <li>{element.content}</li>
-        // </ul>
-      )
-    }
   )
+  
 
   return (
     <div>
       {listItems}
     </div>
-
   )
-
 }
 
 
