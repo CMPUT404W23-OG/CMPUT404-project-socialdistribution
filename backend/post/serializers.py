e437c53b469131f05f19888582f18c0cb96a39fc
KashSansanwal <78171612+KashSansanwal@users.noreply.github.com>
2023-03-03 14:32:26 -0700

    Update serializers.py

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post, Comment
+from .models import Post, Comment, Likes
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,5 +14,6 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Comment
+        model = Likes
         fields = ["id", "summary", "author", "post", "comment"]
+
